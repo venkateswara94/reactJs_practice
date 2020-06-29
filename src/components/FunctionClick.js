006e3e2e9d918ae5d2a9d7850df1fc44c3feaf03
venkateswara94 <venkateswara94@gmail.com>
2020-06-29 23:57:04 +0530

    react practice

--- src/components/FunctionClick.js
+++ src/components/FunctionClick.js
@@ -3,7 +3,7 @@ import React from "react";
 function FunctionClick(){
 
     function clickHandler(){
-        alert("click Handler");
+        alert("click Handler!");
     }
     return (<div>
         <button onClick={clickHandler}>Button Click</button>
